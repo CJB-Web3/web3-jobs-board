5e5db01fd7f9f6b0ffe1ed92654462d31ed61737
Crypto Jobs Board <cryptojobsboard.web3@gmail.com>
2025-06-19 18:53:55 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -164,7 +164,7 @@ export default function Navbar() {
 
                   <Button
                     variant={"ghost"}
-                    onClick={() => handleRedirect("/Pricing")}
+                    onClick={() => handleRedirect("/pricing")}
                   >
                     Pricing
                   </Button>
